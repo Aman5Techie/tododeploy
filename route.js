ae202db9ed44e9517ba9584ed720e4bc6557d50e
Aman5Techie <vermaji9585@gmail.com>
2024-05-25 15:17:08 +0530

    deploy

---
+++ route.js
@@ -0,0 +1,12 @@
+const todosAPI = "https://todos-p3g3.onrender.com/tasks";
+
+const getTodos = todosAPI;
+
+const getTodoById = todosAPI;
+
+const addTodo = todosAPI;
+
+const updateTodo = todosAPI
+
+
+export {todosAPI, getTodoById, getTodos, addTodo,updateTodo };
